c21cfd28acf0bc39489df66de32cce2cc35d418a
tlmohren <tlmohren@nogmail.com>
2021-09-24 17:33:40 -0400

    have single example launch with multiple options

--- launch/example.launch.py
+++ launch/example.launch.py
@@ -1,26 +1,58 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+import ruamel.yaml
+
+ruamel_yaml = ruamel.yaml.YAML(typ="safe")
+ruamel_yaml.default_flow_style = False
 
 
 def generate_launch_description():
     ld = LaunchDescription()
 
-    workspace = get_package_share_directory("flir_camera_driver").split("/install")[0]
-    config = f"{workspace}/src/flir_camera_driver/config/example_config.yaml"
+    ros_package_path = get_package_share_directory("flir_camera_driver")
+    print(f"Ros package path: {ros_package_path}")
 
-    flir_camera = Node(
+    workspace = ros_package_path.split("/install")[0]
+    config_file = f"{workspace}/src/flir_camera_driver/config/example_config.yaml"
+
+    with open(config_file, "r") as yaml_file:
+        example_config = ruamel_yaml.load(yaml_file)
+
+    print(f"\nSelect node name")
+    option_dict = {}
+    default_idx = 0
+    for idx, (node_name, _) in enumerate(example_config.items()):
+        if idx == default_idx:
+            print(f"   {idx}. {node_name} (default)")
+        else:
+            print(f"   {idx}. {node_name}")
+        option_dict[str(idx)] = node_name
+    input_str = input("Select config: ")
+    print(f"")
+
+    try:
+        if input_str == "":
+            input_str = str(default_idx)
+        config_key = option_dict[input_str]
+        cam_config = example_config[config_key]["ros__parameters"]
+        print(f"Launching with config_name: '{config_key}'\n ")
+    except:
+        print(f"Error parsing your input, exiting")
+        exit()
+
+    camera = Node(
         package="flir_camera_driver",
-        executable="publish_camera",
         name="basic_example_camera",
-        parameters=[config],
+        executable="publish_camera",
+        parameters=[cam_config],
     )
-    ld.add_action(flir_camera)
+    ld.add_action(camera)
 
     rqt_viewer = Node(
         package="rqt_image_view",
-        executable="rqt_image_view",
         name="image_preview",
+        executable="rqt_image_view",
         arguments=["/camera/image_mono"],
     )
     ld.add_action(rqt_viewer)
