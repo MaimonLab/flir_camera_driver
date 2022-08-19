336fbac0089eda206ca4b0066c98febd45a8fc1f
tlmohren <tlmohren@nogmail.com>
2022-08-19 16:40:47 -0400

    v0.1.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ package_name = "flir_camera_driver"
 
 setup(
     name=package_name,
-    version="0.1.3",
+    version="0.1.4",
     packages=[package_name],
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
