bf141c09ecc2cf5dfb5d71739b37190e7d9a4882
Stephen Thornquist <thornquist.stephen@gmail.com>
2024-12-31 15:22:06 -0500

    still a crashing disaster but just barely usable...

--- flir_camera_driver/display_each_camera.py
+++ flir_camera_driver/display_each_camera.py
@@ -16,7 +16,10 @@ def main():
     cameras = _system.GetCameras()
     if not cameras.GetSize():
         raise RuntimeError("No cameras found!")
-    for idx, cam in enumerate(cameras):
+    # for idx, cam in enumerate(cameras):
+    idx = 0
+    while True:
+        idx = idx % cameras.GetSize()
         roscam = Camera(cam_id=idx)
         print(f"Camera {idx}: {roscam.get_attr('DeviceSerialNumber')}")
         roscam.open_cam()
@@ -26,38 +29,43 @@ def main():
         roscam.set_attr("Binning Vertical", 2)
         roscam.start()
         while True:
-            # immutable
-            img_cv, _ = roscam.get_new_frame(get_chunk=False)
-            height = len(img_cv) 
-            # make a mutable version of img_cv
-            img_cv = img_cv.copy()
-            cv2.putText(
-                img_cv,
-                f"Press N for next Camera, Q to quit.",
-                (0, height // 5),
-                cv2.FONT_HERSHEY_COMPLEX,
-                1,
-                (255, 255, 255),
-                1,
-            )
+            try:
+                # immutable
+                img_cv, _ = roscam.get_new_frame(get_chunk=False)
+                height = len(img_cv) 
+                # make a mutable version of img_cv
+                img_cv = img_cv.copy()
+                cv2.putText(
+                    img_cv,
+                    f"Press N for next Camera, Q to quit.",
+                    (0, height // 5),
+                    cv2.FONT_HERSHEY_COMPLEX,
+                    1,
+                    (255, 255, 255),
+                    1,
+                )
 
-            cv2.putText(
-                img_cv,
-                f"Camera: {roscam.get_attr('DeviceSerialNumber')}",
-                (0, height // 2),
-                cv2.FONT_HERSHEY_COMPLEX,
-                1.8,
-                (255, 255, 255),
-                2,
-            )
+                cv2.putText(
+                    img_cv,
+                    f"Camera: {roscam.get_attr('DeviceSerialNumber')}",
+                    (0, height // 2),
+                    cv2.FONT_HERSHEY_COMPLEX,
+                    1.8,
+                    (255, 255, 255),
+                    2,
+                )
 
-            cv2.imshow("image", img_cv)
+                cv2.imshow("image", img_cv)
+            except Exception:
+                print(f"Error in camera {idx}")
+                break
             key = cv2.waitKey(1)
             if key & 0xFF == ord("n"):
                 break
             if key & 0xFF == ord("q"):
                 return
         roscam.destroy() # RIP BOZO
+        idx += 1
     _system.ReleaseInstance()
     print("Done!")
     # break
